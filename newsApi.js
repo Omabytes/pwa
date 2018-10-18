b6b3547f863c62702a087ce5a1fe244288b30590
omabytes <321.trebor@gmail.com>
2018-10-18 14:49:07 +0100

    news API details file

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = b8a820a0e2d84293a501c4b3cbf6fb5e
+
+const topHeadlinesURL = 'https://newsapi.org/v2/top-headlines?country=gb&category=technology&apiKey=' + apiKey
+
+export { apiKey, topHeadlinesURL }
